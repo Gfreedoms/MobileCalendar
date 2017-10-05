b56485a835452c73a5a9d300bc69a90c9cb8389d
Gfreedoms <freedomgemmar1@gmail.com>
2017-10-05 11:43:03 +0300

    remove repetetive code

--- src/components/MobileCalendarContainer.ts
+++ src/components/MobileCalendarContainer.ts
@@ -52,6 +52,7 @@ export default class MobileCalendarContainer extends Component<ContainerProps, C
         this.subscribe(this.props.mxObject);
         this.handleClick = this.handleClick.bind(this);
         this.displayInfo = this.displayInfo.bind(this);
+        this.handleForm = this.handleForm.bind(this);
     }
 
     render() {
@@ -94,45 +95,49 @@ export default class MobileCalendarContainer extends Component<ContainerProps, C
     }
 
     private displayInfo() {
-        const today = new Date();
-        const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
         const displayTextBox = createElement("div", {},
-          createElement("input", {
-                type: "text",
-                className: "form-control",
-                placeholder: this.state.printdate,
-                onClick: this.handleClick
-            })
+          this.handleForm()
         );
         const displayCalendar = createElement("div", {},
-            createElement("input", {
-                type: "text",
-                className: "form-control",
-                placeholder: this.state.printdate,
-                onClick: this.handleClick
-            }),
-            createElement(InfiniteCalendar, {
-
-                onSelect: (date: string) => this.setState({ printdate: `${format(date, "ddd, MMM Do YYYY")}` }),
-                disabledDays: [ 0, 6 ],
-                minDate: lastWeek,
-                layout: this.props.layout,
-                width: this.props.width,
-                height: this.props.height,
-                showHeader: this.props.showHeader,
-                showOverlay: this.props.showOverlay,
-                hideYearsOnSelect: this.props.hideYearsOnSelect,
-                todayHelperRowOffset: this.props.todayHelperRowOffset,
-                shouldHeaderAnimate: this.props.shouldHeaderAnimate,
-                rowHeight: this.props.rowHeight,
-                autoFocus: this.props.autoFocus,
-                tabIndex: this.props.tabIndex,
-                display: this.props.display
-            })
+        this.handleForm()
+            ,
+            this.handleCalendar()
         );
         return createElement("div", {},
             this.state.isPlainText ? displayTextBox : displayCalendar
         );
 
     }
+
+    private handleForm() {
+         return (createElement("input", {
+                type: "text",
+                className: "form-control",
+                placeholder: this.state.printdate,
+                onClick: this.handleClick
+            }));
+    }
+
+    private handleCalendar() {
+        const today = new Date();
+        const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        return (createElement(InfiniteCalendar, {
+            onSelect: (date: string) => this.setState({ printdate: `${format(date, "ddd, MMM Do YYYY")}` }),
+            disabledDays: [ 0, 6 ],
+            minDate: lastWeek,
+            layout: this.props.layout,
+            width: this.props.width,
+            height: this.props.height,
+            showHeader: this.props.showHeader,
+            showOverlay: this.props.showOverlay,
+            hideYearsOnSelect: this.props.hideYearsOnSelect,
+            todayHelperRowOffset: this.props.todayHelperRowOffset,
+            shouldHeaderAnimate: this.props.shouldHeaderAnimate,
+            rowHeight: this.props.rowHeight,
+            autoFocus: this.props.autoFocus,
+            tabIndex: this.props.tabIndex,
+            display: this.props.display
+        }));
+    }
+
    }
